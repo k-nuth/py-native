7dbbbac7fe702f27f6b37284c9cb6cf42909c1d1
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-23 10:04:50 -0300

    test py3 on osx

--- setup.py
+++ setup.py
@@ -317,9 +317,9 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
