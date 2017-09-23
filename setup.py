bc37d95593bc9b691113004a4d06ce542037f572
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-09-23 08:45:52 -0300

    fixes and version update

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ import os
 
         
 PKG_NAME = 'bitprim_native'
-VERSION = '1.0.55'
+VERSION = '1.0.56'
 SYSTEM = sys.platform
 
 def get_similar_lib(path, pattern):
@@ -200,7 +200,7 @@ extensions = [
         define_macros = [('BITPRIM_LIB_STATIC', None),],
 
     	sources = ['utils.c',  'chain/header.c', 'chain/block.c', 'chain/merkle_block.c', 'bitprimmodule.cpp',
-        'chain/chain.c', 'binary.c', 'chain/point.c', 'chain/history.c', 'chain/word_list.c', 
+        'chain/tools.cpp', 'chain/chain.c', 'binary.c', 'chain/point.c', 'chain/history.c', 'chain/word_list.c', 
         'chain/transaction.c', 'chain/output.c', 'chain/output_list.c',  'chain/input.c', 'chain/input_list.c', 
         'chain/script.c', 'chain/payment_address.c', 'chain/compact_block.c', 'chain/output_point.c',
         'chain/block_list.c', 'chain/transaction_list.c', 'chain/stealth_compact.c', 'chain/stealth_compact_list.c'],
