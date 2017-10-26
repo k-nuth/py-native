4681a1b51ea08ff03906d4213c42f42af66c8973
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-10-26 17:31:57 -0300

    code style

--- chain/block.h
+++ chain/block.h
@@ -19,19 +19,19 @@ PyObject* bitprim_native_chain_block_reward(PyObject* self, PyObject* args);
 PyObject* bitprim_native_chain_block_generate_merkle_root(PyObject* self, PyObject* args);
 PyObject* bitprim_native_chain_block_hash(PyObject* self, PyObject* args);
 
-PyObject * bitprim_native_chain_block_is_valid(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_transaction_nth(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_signature_operations(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_signature_operations_bip16_active(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_total_inputs(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_is_extra_coinbases(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_is_final(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_is_distinct_transaction_set(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_is_valid_coinbase_claim(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_is_valid_coinbase_script(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_is_internal_double_spend(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_is_valid_merkle_root(PyObject* self, PyObject* args);
-PyObject * bitprim_native_chain_block_destruct(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_is_valid(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_transaction_nth(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_signature_operations(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_signature_operations_bip16_active(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_total_inputs(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_is_extra_coinbases(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_is_final(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_is_distinct_transaction_set(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_is_valid_coinbase_claim(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_is_valid_coinbase_script(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_is_internal_double_spend(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_is_valid_merkle_root(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_block_destruct(PyObject* self, PyObject* args);
 
 #ifdef __cplusplus
 } //extern "C"
