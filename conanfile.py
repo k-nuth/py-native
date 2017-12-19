37dc06f910689fc3fe8f1d102c9c60a6ca35c8ac
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-12-19 13:29:06 -0300

    using node-cint v0.4

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,7 @@ class BitprimTestForPy(ConanFile):
     # build_policy = "missing"
 
     # TODO(fernando): queda pendiente seleccionar el default Shared=False
-    requires = (("bitprim-node-cint/0.3@bitprim/testing"))
+    requires = (("bitprim-node-cint/0.4@bitprim/testing"))
     # default_options = "bitprim-node-cint:shared=False" #, "OpenSSL:shared=True"
 
 
