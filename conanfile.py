64340162e74216c04c578bd04d6ac794d4cecaaa
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-08-07 11:09:55 -0300

    pointing to Bitprim 0.12.0

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,7 @@ class BitprimTestForPy(ConanFile):
     # build_policy = "missing"
 
     # TODO(fernando): queda pendiente seleccionar el default Shared=False
-    requires = (("bitprim-node-cint/0.10@bitprim/stable"))
+    requires = (("bitprim-node-cint/0.12.0@bitprim/stable"))
     # default_options = "bitprim-node-cint:shared=False" #, "OpenSSL:shared=True"
 
 
