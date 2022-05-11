3798633cc79ac2c0ea4046bb0fb481d9cc6944c2
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-05-11 06:03:14 -0300

    WIP

--- .travis/entrypoint.sh
+++ .travis/entrypoint.sh
@@ -7,7 +7,8 @@ echo "-----------------------------------------------------------------"
 
 python --version
 python3 --version
-
+pyenv --version
+pyenv install --list
 
 echo "-----------------------------------------------------------------"
 echo "-----------------------------------------------------------------"
@@ -21,8 +22,9 @@ sudo pip install --upgrade wheel
 sudo pip install --upgrade twine
 
 conan user
-
-conan remote add kth_temp https://knuth.jfrog.io/artifactory/api/conan/knuth
+# conan remote add kth_temp https://knuth.jfrog.io/artifactory/api/conan/knuth
+conan config install https://github.com/k-nuth/ci-utils/raw/master/conan/config.zip
+conan profile update settings.compiler.libcxx=libstdc++11 default
 
 cd /home/conan/project
 sudo conan install .
