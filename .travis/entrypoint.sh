40e8e1d3c33edfec41ab34c7d519cf58ddb31559
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-05-30 01:01:34 +0200

    WIP 2

--- .travis/entrypoint.sh
+++ .travis/entrypoint.sh
@@ -31,9 +31,9 @@ echo "-----------------------------------------------------------------"
 echo "-----------------------------------------------------------------"
 echo "-----------------------------------------------------------------"
 
-wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz
-tar -xf Python-3.10.4.tgz
-cd Python-3.10.4
+wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
+tar -xf Python-$PYTHON_VERSION.tgz
+cd Python-$PYTHON_VERSION
 ./configure --prefix=$HOME/opt --with-openssl=$HOME/opt/ssl --with-openssl-rpath=auto && make && make install
 
 
@@ -112,6 +112,30 @@ echo "-----------------------------------------------------------------"
 
 cd /home/conan/project
 
+
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+
+PROJECT_VERSION="$(python -c 'from version import __version__; print(__version__)')"
+
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+
+echo $PROJECT_VERSION
+
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+
+
 # sudo conan install .
 # sudo pip install  -e .
 # sudo python setup.py sdist
@@ -124,10 +148,12 @@ conan install .
 echo "*************************************************"
 ls
 echo "*************************************************"
-auditwheel show dist/kth_py_native-0.1.0-cp310-cp310-linux_x86_64.whl
+auditwheel show dist/kth_py_native-0.1.0-$CP_PYTHON_VERSION-$CP_PYTHON_VERSION-linux_x86_64.whl
 echo "*************************************************"
-auditwheel repair dist/kth_py_native-0.1.0-cp310-cp310-linux_x86_64.whl
-# rm dist/kth_py_native-0.1.0-cp310-cp310-linux_x86_64.whl
+auditwheel repair dist/kth_py_native-0.1.0-$CP_PYTHON_VERSION-$CP_PYTHON_VERSION-linux_x86_64.whl
+
+
+# rm dist/kth_py_native-0.1.0-$CP_PYTHON_VERSION-$CP_PYTHON_VERSION-linux_x86_64.whl
 # echo "*************************************************"
 # ls
 # echo "*************************************************"
@@ -135,7 +161,7 @@ auditwheel repair dist/kth_py_native-0.1.0-cp310-cp310-linux_x86_64.whl
 # echo "*************************************************"
 # ls wheelhouse
 # echo "*************************************************"
-# auditwheel show dist/kth_py_native-0.1.0-cp310-cp310-linux_x86_64.whl
+# auditwheel show dist/kth_py_native-0.1.0-$CP_PYTHON_VERSION-$CP_PYTHON_VERSION-linux_x86_64.whl
 # auditwheel show wheelhouse/kth_py_native-0.1.0-cp35-cp35m-manylinux_2_24_x86_64.whl
 # echo "*************************************************"
 
