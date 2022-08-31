25f651e7369cb13d98faf8afc1bb74945d918e68
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-08-31 16:52:29 +0200

    v0.5.0

--- version.py
+++ version.py
@@ -4,4 +4,4 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
